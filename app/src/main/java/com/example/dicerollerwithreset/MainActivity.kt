73e817f3bebe896140aae643829818bbd533b9b5
Deepika G P <deepgpika00@gmail.com>
2021-08-06 00:29:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dicerollerwithreset/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.dicerollerwithreset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { resetting() }
+    }
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+    }
+
+        val count: Int = 0
+
+    private fun resetting(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text=count.toString()
+
+    }
+
+}
+
+
